d8ad7a703f8ad9840e8784c7fafc7edd7d3411d0
Zafar Saleem <zafarsaleem3@gmail.com>
2019-01-21 16:20:07 -0500

    fixed table header on employee list

--- src/components/admin/employeesList/employeesListView.js
+++ src/components/admin/employeesList/employeesListView.js
@@ -8,30 +8,30 @@ const EmployeesListView = (props) => {
 
   return (
     <div>
-      {props.list.map(li => (
-        <table key={li._id}>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Position</th>
-              <th>Username</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <Link to={`/employee/details/${li._id}`}>{li.name}</Link>
-              </td>
-              <td>
-                {li.position}
-              </td>
-              <td>
-                {li.username}
-              </td>
-            </tr>
-          </tbody>
-        </table>        
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+      {props.list.map(li => (  
+        <tbody key={li._id}>
+          <tr>
+            <td>
+              <Link to={`/employee/details/${li._id}`}>{li.name}</Link>
+            </td>
+            <td>
+              {li.position}
+            </td>
+            <td>
+              {li.username}
+            </td>
+          </tr>
+        </tbody>
       ))}
+      </table>
     </div>
   );
 };
