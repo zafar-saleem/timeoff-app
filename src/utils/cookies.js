a65b738e10f6242a261cf9aa6241d36da940da9f
Zafar Saleem <zafarsaleem3@gmail.com>
2019-01-26 12:55:03 -0500

    changed cookies expiry from day to hours

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,6 +1,6 @@
-export function setCookie(cname, cvalue, exdays) {
+export function setCookie(cname, cvalue, hours) {
   let d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  d.setTime(d.getTime() + (hours * 60 * 60 * 1000)); // (exdays * 24 * 60 * 60 * 1000));
   let expires = 'expires=' + d.toUTCString();
   document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
 }
