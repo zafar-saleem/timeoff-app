96839319192f5d4c9a6035d48ebc5652619ba33e
Zafar Saleem <zafarsaleem3@gmail.com>
2019-01-26 21:12:41 -0500

    employee navigation links are added to header

--- src/components/commons/headerComponent.js
+++ src/components/commons/headerComponent.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import { getCookie } from '../../utils/cookies';
+
 class HeaderComponent extends Component {
   render() {
     return (
       <div>
+        {(getCookie('role') === 'Admin')
+        ?
         <ul>
           <li><Link to="/admin/list">Employees</Link></li>
           <li><Link to="/admin/new">Add</Link></li>
           <li><Link to="/admin/dashboard">Dashboard</Link></li>
         </ul>
+        :
+        <ul>
+          <li><Link to="/employee/home">Home</Link></li>
+          <li><Link to="/employee/profile">Profile</Link></li>
+        </ul>
+        }
         <Link to="/logout">Logout</Link>
       </div>
     );
