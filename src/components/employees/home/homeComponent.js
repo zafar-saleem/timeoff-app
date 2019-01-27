25d82713b164f096e74fab5ba848539b1eed380b
Zafar Saleem <zafarsaleem3@gmail.com>
2019-01-27 05:47:15 -0500

    home component for employees with sample text

---
+++ src/components/employees/home/homeComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import HeaderComponent from '../../commons/headerComponent';
+
+class HomeComponent extends Component {
+  render() {
+    return (
+      <div>
+        <HeaderComponent />
+        HRRLOOOOO
+      </div>
+    )
+  }
+}
+
+export default HomeComponent;
