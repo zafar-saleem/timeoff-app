cece676cae2b2ce4efa017a6b693f8f046501712
Zafar Saleem <zafarsaleem3@gmail.com>
2019-01-27 19:25:14 -0500

    added calendar on employee home page

--- src/components/employees/home/homeComponent.js
+++ src/components/employees/home/homeComponent.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Calendar from 'react-calendar';
 
 import HeaderComponent from '../../commons/headerComponent';
 
 class HomeComponent extends Component {
+  onChange(date) {
+    console.log(date);
+  }
+
   render() {
     return (
       <div>
         <HeaderComponent />
-        HRRLOOOOO
+        <Calendar
+          onChange={this.onChange}
+          selectRange={true}
+        />
       </div>
     )
   }
