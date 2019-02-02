f1d1a209e27c474d9c82ab2b803ea3be021ead13
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-02 12:33:52 -0500

    styled Employee profile page

--- src/components/employees/profile/profileView.js
+++ src/components/employees/profile/profileView.js
@@ -3,15 +3,36 @@ import { Link } from 'react-router-dom';
 
 import { getCookie } from '../../../utils/cookies';
 
-const ProfileView = (props) => (
-  <div>
-    <ul>
-      <li>Name: {props.employee.name}</li>
-      <li>Position: {props.employee.position}</li>
-      <li>Role: {props.employee.role}</li>
-      <li>Username: {props.employee.username}</li>
-    </ul>
-    <Link to={`/employee/update/${getCookie('id')}`}>Edit</Link>
+import './profile.css';
+
+const ProfileView = ({employee}) => (
+  <div className='profile'>
+    <table>
+      <tbody>
+          <tr>
+            <th>Name</th><td>{employee.name}</td>
+          </tr>
+          <tr>
+            <th>Position</th><td>{employee.position}</td>
+          </tr>
+          <tr>
+            <th>Role</th><td>{employee.role}</td>
+          </tr>
+          <tr>
+            <th>Username</th><td>{employee.username}</td>
+          </tr>
+          {(employee.active !== false)
+            ? 
+            <tr className='edit-link'>
+              <th></th><Link to={`/employee/update/${getCookie('id')}`} className='edit'>Edit</Link>
+            </tr>
+            : null
+          }
+      </tbody>
+    </table>
+
+
+    
   </div>
 );
 
