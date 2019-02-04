d119bc408929720a91e8e9504d13dd905129bfda
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-03 21:03:44 -0500

    set expire className based on expired vacation

--- src/components/employees/home/homeView.js
+++ src/components/employees/home/homeView.js
@@ -8,8 +8,9 @@ import { getCookie } from '../../../utils/cookies';
 class HomeView extends Component {
 
   render() {
+    console.log(this.props.vacation);
     return (
-      <tr>
+      <tr className={(this.props.vacation.expire) ? 'expire' : ''}>
         You are going on vacation from <b>{this.props.vacation.start}</b> to <b>{this.props.vacation.end}</b>.
         <Link to='' className='btn-delete' onClick={this.props.handleDelete.bind(this, this.props.vacation._id)}>Delete</Link>
       </tr>
