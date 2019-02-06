1c5f8da2a859f2214ef814f30db5075644e14ff7
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-06 13:45:21 -0500

    send user role upon logout

--- src/components/authentications/logoutComponent.js
+++ src/components/authentications/logoutComponent.js
@@ -9,7 +9,7 @@ class LogoutComponent extends Component {
   constructor(props) {
     super(props);
     
-    this.props.dispatch(logoutAction({id: getCookie('id')}));
+    this.props.dispatch(logoutAction({id: getCookie('id'), access: getCookie('role')}));
 
     setCookie('token', getCookie('token'), 0);
     setCookie('role', getCookie('role'), 0);
