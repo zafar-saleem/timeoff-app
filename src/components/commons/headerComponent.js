9a01b1a133525ab6a1b20464451f63f89d8b15b1
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-07 12:29:12 -0500

    replaced inverted commas with single quote

--- src/components/commons/headerComponent.js
+++ src/components/commons/headerComponent.js
@@ -14,20 +14,20 @@ class HeaderComponent extends Component {
         {(getCookie('role') === 'Admin')
         ?
         <ul className='navbar'>
-          <li><Link className='navlink active' to="/admin/dashboard">Dashboard</Link></li>
-          <li><Link className='navlink' to="/admin/list">Employees</Link></li>
-          <li><Link className='navlink' to="/admin/new">Add Employee</Link></li>
+          <li><Link className='navlink active' to='/admin/dashboard'>Dashboard</Link></li>
+          <li><Link className='navlink' to='/admin/list'>Employees</Link></li>
+          <li><Link className='navlink' to='/admin/new'>Add Employee</Link></li>
         </ul>
         :
         <ul className='navbar'>
-          <li><Link className='navlink active' to="/employee/home">Home</Link></li>
-          <li><Link className='navlink' to="/employee/profile">Profile</Link></li>
+          <li><Link className='navlink active' to='/employee/home'>Home</Link></li>
+          <li><Link className='navlink' to='/employee/profile'>Profile</Link></li>
         </ul>
         }
         {(getCookie('role') === 'Admin')
-        ? <span className='welcome uppercase'>Welcome <Link to="/admin/profile" className="link success">Admin</Link></span> 
-        : <span className='welcome uppercase'>Welcome <Link to="/employee/profile" className="link success">{getCookie('name')}</Link></span>}
-        <Link to="/logout" className='btn danger logout'>Logout</Link>
+        ? <span className='welcome uppercase'>Welcome <Link to='/admin/profile' className='link success'>Admin</Link></span> 
+        : <span className='welcome uppercase'>Welcome <Link to='/employee/profile' className='link success'>{getCookie('name')}</Link></span>}
+        <Link to='/logout' className='btn danger logout'>Logout</Link>
       </div>
     );
   }
