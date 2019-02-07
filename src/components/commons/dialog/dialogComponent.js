904b274cceb7f87ea79d56ac9aa3cf22601f45e7
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-07 14:00:29 -0500

    FIX: Changed dialog buttons background colors and place them

--- src/components/commons/dialog/dialogComponent.js
+++ src/components/commons/dialog/dialogComponent.js
@@ -9,8 +9,8 @@ class DialogComponent extends Component {
       <div className={(this.props.warning) ? 'show' : 'hide'}>
         <div className='dialog'>
           {this.props.message}
-          <button className='btn success btn-yes' onClick={this.props.callback}>Yes</button>
-          <button className='btn danger btn-no' onClick={this.props.callback}>No</button>
+          <button className='btn danger btn-yes' onClick={this.props.callback}>Yes</button>
+          <button className='btn success btn-no' onClick={this.props.callback}>No</button>
         </div>
         <div className='backdrop'></div>
       </div>
