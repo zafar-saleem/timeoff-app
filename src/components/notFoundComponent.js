a481b6c3f94a75c19ff32b1adb3abc8adcef6321
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-21 15:36:43 -0500

    TASK: Updated text for not found

--- src/components/notFoundComponent.js
+++ src/components/notFoundComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './notFound.css';
 
-const Notfound = () => <h1 className='notfound'>Not found</h1>
+const Notfound = () => <h1 className='notfound'>URL Not found</h1>
 
 export default Notfound
